8b088da127f9e4698f69adaf3ad01ee3c1de4e4b
HollowTM <steffenxhennig@gmail.com>
2016-06-25 15:59:30 +0200

    Add files via upload
    Console hinzugefügt in myMain
    die zusätzlichen Aufgaben wieder eingebunden
    Kommentare hinzugefügt

---
+++ src/main/java/BinaryPicture.java
@@ -0,0 +1,188 @@
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+
+/**
+ * Implementierung und Testen eines Binary Pictures mithilfe der Methode von Otsu 
+ * https://en.wikipedia.org/wiki/Otsu%27s_method
+ * 
+ * @author Hollow
+ *
+ */
+public class BinaryPicture {
+
+	private static BufferedImage grayscale, binarized;
+
+	public BinaryPicture() {
+
+	}
+
+	public BufferedImage createBinaryPicture(BufferedImage original) {
+		grayscale = toGray(original);
+		binarized = binarize(grayscale);
+		return binarized;
+	}
+
+	/**
+	 * Histogramm erzeugen 
+	 * @param input
+	 * @return
+	 */
+	public static int[] imageHistogram(BufferedImage input) {
+
+		int[] histogram = new int[256];
+
+		for (int i = 0; i < histogram.length; i++)
+			histogram[i] = 0;
+
+		for (int i = 0; i < input.getWidth(); i++) {
+			for (int j = 0; j < input.getHeight(); j++) {
+				int red = new Color(input.getRGB(i, j)).getRed();
+				histogram[red]++;
+			}
+		}
+
+		return histogram;
+
+	}
+
+	/**
+	 * Pixel umwandeln von Color zu Gray
+	 * @param original
+	 * @return
+	 */
+	private static BufferedImage toGray(BufferedImage original) {
+
+		int alpha, red, green, blue;
+		int newPixel;
+
+		BufferedImage lum = new BufferedImage(original.getWidth(), original.getHeight(), original.getType());
+
+		for (int i = 0; i < original.getWidth(); i++) {
+			for (int j = 0; j < original.getHeight(); j++) {
+
+				// Get pixels by R, G, B
+				alpha = new Color(original.getRGB(i, j)).getAlpha();
+				red = new Color(original.getRGB(i, j)).getRed();
+				green = new Color(original.getRGB(i, j)).getGreen();
+				blue = new Color(original.getRGB(i, j)).getBlue();
+
+				red = (int) (0.21 * red + 0.71 * green + 0.07 * blue);
+				// Return back to original format
+				newPixel = colorToRGB(alpha, red, red, red);
+
+				// Write pixels into image
+				lum.setRGB(i, j, newPixel);
+
+			}
+		}
+
+		return lum;
+
+	}
+
+	/**
+	 * Berechne den Treshold mithilfe der Methode von Otsu
+	 * @param original
+	 * @return
+	 */
+	private static int otsuTreshold(BufferedImage original) {
+
+		int[] histogram = imageHistogram(original);
+		int total = original.getHeight() * original.getWidth();
+
+		float sum = 0;
+		for (int i = 0; i < 256; i++)
+			sum += i * histogram[i];
+
+		float sumB = 0;
+		int wB = 0;
+		int wF = 0;
+
+		float varMax = 0;
+		int threshold = 0;
+
+		for (int i = 0; i < 256; i++) {
+			wB += histogram[i];
+			if (wB == 0)
+				continue;
+			wF = total - wB;
+
+			if (wF == 0)
+				break;
+
+			sumB += (float) (i * histogram[i]);
+			float mB = sumB / wB;
+			float mF = (sum - sumB) / wF;
+
+			float varBetween = (float) wB * (float) wF * (mB - mF) * (mB - mF);
+
+			if (varBetween > varMax) {
+				varMax = varBetween;
+				threshold = i;
+			}
+		}
+
+		return threshold;
+
+	}
+
+	/**
+	 * Entscheide ob Pixel Schwarz oder Weiﬂ ist. binarize
+	 * @param original
+	 * @return
+	 */
+	private static BufferedImage binarize(BufferedImage original) {
+
+		int red;
+		int newPixel;
+
+		int threshold = otsuTreshold(original);
+
+		BufferedImage binarized = new BufferedImage(original.getWidth(), original.getHeight(), original.getType());
+
+		for (int i = 0; i < original.getWidth(); i++) {
+			for (int j = 0; j < original.getHeight(); j++) {
+
+				// Get pixels
+				red = new Color(original.getRGB(i, j)).getRed();
+				int alpha = new Color(original.getRGB(i, j)).getAlpha();
+				if (red > threshold) {
+					newPixel = 255;
+				} else {
+					newPixel = 0;
+				}
+				newPixel = colorToRGB(alpha, newPixel, newPixel, newPixel);
+				binarized.setRGB(i, j, newPixel);
+
+			}
+		}
+
+		return binarized;
+
+	}
+
+	/**
+	 * Konvertierung von R,G,B,Alpha nach Standard 8-bit
+	 * Convert R, G, B, Alpha to standard 8 bit
+	 * @param alpha
+	 * @param red
+	 * @param green
+	 * @param blue
+	 * @return
+	 */
+	private static int colorToRGB(int alpha, int red, int green, int blue) {
+
+		int newPixel = 0;
+		newPixel += alpha;
+		newPixel = newPixel << 8;
+		newPixel += red;
+		newPixel = newPixel << 8;
+		newPixel += green;
+		newPixel = newPixel << 8;
+		newPixel += blue;
+
+		return newPixel;
+
+	}
+
+}
--- src/main/java/myMain.java
+++ src/main/java/myMain.java
@@ -1,7 +1,11 @@
 import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
+import java.awt.image.ColorConvertOp;
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 import java.util.concurrent.ForkJoinPool;
+import java.awt.color.ColorSpace;
 
 import javax.imageio.ImageIO;
 
@@ -10,14 +14,155 @@ public class myMain {
 	// Bild-Datei-Verwaltung, Abfrage der zu verwendeten Matrix und Entscheidung
 	// parallel/sec in der Main
 	public static void main(String[] args) throws IOException {
-		
-		System.out.println("todo: Objektnamen sollen mit einem gro√üen Buchstaben beginnen");
-		System.out.println("todo: (String?) Konstanten ausfindig machen, diese Gro√ü schreiben und mit final versehen");
+
+		System.out.println("todo: Objektnamen sollen mit einem groﬂen Buchstaben beginnen");
+		System.out.println("todo: (String?) Konstanten ausfindig machen, diese Groﬂ schreiben und mit final versehen");
 		System.out.println("todo: type filename?, threadcounter, mode, matrix and threshold");
 		System.out.println("todo: tests");
 		System.out.println("todo: Kommentare/Dokumentation!!!");
-		System.out.println("todo - sobald alles andere l√§uft: l√∂sche thread counter aus der Main und den anderen Objekten");
-		
+		System.out.println("todo - sobald alles andere l‰uft: lˆsche thread counter aus der Main und den anderen Objekten");
+		System.out.println("todo - ‹berpr¸ft nochmal die Matrix!!!!!!!!!!!!!!!!!!!!!!!!!!!! Meine bei mir gehen nicht alle");
+
+		boolean quit = false;
+		boolean seqPara;
+		Scanner inputReader = new Scanner(System.in);
+		String input = null;
+
+		while (!quit) {
+			System.out.println("Bitte Verfahren w‰hlen:");
+			System.out.println("1: FloydSteinberg algorithmus sequenziell");
+			System.out.println("2: FloydSteinberg algorithmus parallel");
+			System.out.println(
+					"3: Verschiedene Test Bilder \n Verfahren: GrayScale, GrayScale mit ColorConverter, BinaryPicture, Ordered Dither ");
+			input = inputReader.nextLine();
+
+			if (input.equals("1")) {
+				input = inputReader.nextLine();
+				seqPara = false;
+				startFloySteinberg(seqPara, 0, 0);
+
+			} else if (input.equals("2")) {
+				// Nachfragen nach Methoden
+				int mode = 0;
+				int thread = 0;
+				System.out.println("Anzahl Threads angeben:");
+				input = inputReader.nextLine();
+				thread = Integer.parseInt(input);
+
+				System.out.println("Auswahl Modus:");
+				input = inputReader.nextLine();
+				mode = Integer.parseInt(input);
+
+				seqPara = true;
+				startFloySteinberg(seqPara, thread, mode);
+
+			} else if (input.equals("3")) {
+				createGrayScalePics();
+
+			} else if (input.equals("exit"))
+				inputReader.close();
+			quit = true;
+
+		}
+
+	}
+
+	/**
+	 * Erzeugt die zus‰tzlichen Bilder die nicht Teil der Aufgabe waren Diese
+	 * werden der Reihe nach einfach ausgegeben Zuerst Binary Picture Dann ein
+	 * GrayScale Picture mit dem Colour Converter Ordered Dither
+	 * 
+	 * @throws IOException
+	 */
+	public static void createGrayScalePics() throws IOException {
+		final String PATH = "src/main/resources/";
+		final String SOURCE = "example.jpg";
+		String RESULTNAME = null;
+		final String RESULTTYPE = "jpg";
+
+		BufferedImage sourceImg = ImageIO.read(new File(PATH + SOURCE));
+		BufferedImage resultImg;
+
+		// Binary Picture
+		RESULTNAME = "BinaryPicture";
+		resultImg = new BinaryPicture().createBinaryPicture(sourceImg);
+		ImageIO.write(resultImg, RESULTTYPE, new File(PATH + RESULTNAME + "." + RESULTTYPE));
+		System.out.println("BinaryPicture: " + RESULTNAME + "." + RESULTTYPE);
+
+		// GrayScale ColorConverter Picture
+		RESULTNAME = "GrayScaleColorConverter";
+		resultImg = makeGrayBuffered(sourceImg);
+		ImageIO.write(resultImg, RESULTTYPE, new File(PATH + RESULTNAME + "." + RESULTTYPE));
+		System.out.println("GrayScale: " + RESULTNAME + "." + RESULTTYPE);
+
+		// GrayScale Picture
+		RESULTNAME = "GrayScale";
+		resultImg = makeGray(sourceImg);
+		ImageIO.write(resultImg, RESULTTYPE, new File(PATH + RESULTNAME + "." + RESULTTYPE));
+		System.out.println("GrayScale: " + RESULTNAME + "." + RESULTTYPE);
+
+		// Ordered Dither
+		RESULTNAME = "OrderedDither";
+		resultImg = new OrderedDither().createBinaryPicture(sourceImg);
+		ImageIO.write(resultImg, RESULTTYPE, new File(PATH + RESULTNAME + "." + RESULTTYPE));
+		System.out.println("OrderedDither: " + RESULTNAME + "." + RESULTTYPE);
+
+	}
+
+	/**
+	 * Gray Scale Converter Testen anderer Methode um Umgang mit BufferedImage
+	 * zu bekommen
+	 * http://stackoverflow.com/questions/9131678/convert-a-rgb-image-to-
+	 * grayscale-image-reducing-the-memory-in-java
+	 * 
+	 * @param img
+	 * @return
+	 */
+	public static BufferedImage makeGray(BufferedImage img) {
+		for (int x = 0; x < img.getWidth(); ++x)
+			for (int y = 0; y < img.getHeight(); ++y) {
+				int rgb = img.getRGB(x, y);
+				int r = (rgb >> 16) & 0xFF;
+				int g = (rgb >> 8) & 0xFF;
+				int b = (rgb & 0xFF);
+
+				int grayLevel = (r + g + b) / 3;
+				int gray = (grayLevel << 16) + (grayLevel << 8) + grayLevel;
+				img.setRGB(x, y, gray);
+			}
+
+		return img;
+	}
+
+	/**
+	 * Gray Scale ColorConvert Option zum Testen verschiedener Grayscale
+	 * Methoden
+	 * http://codehustler.org/blog/java-to-create-grayscale-images-icons/
+	 * 
+	 * @param img
+	 * @return Grayscale Bild
+	 */
+	public static BufferedImage makeGrayBuffered(BufferedImage img) {
+		// Gray Scale
+		BufferedImageOp op = new ColorConvertOp(ColorSpace.getInstance(ColorSpace.CS_GRAY), null);
+		BufferedImage image = op.filter(img, null);
+
+		return image;
+	}
+
+	/**
+	 * Methode f¸r den Floy-Steinberg Algorithmus Bei der ‹bergabe wird
+	 * entschieden ob es die Methode sequentiell oder Parallel ausgef¸hrt werden
+	 * soll Mit wie vielen Thread die Parallelmethode gestartet wird und in
+	 * welchem Matrix Modus.
+	 * 
+	 * @param seqPara
+	 * @param thread
+	 * @param mode
+	 * @throws IOException
+	 */
+	public static void startFloySteinberg(boolean seqPara, int thread, int mode) throws IOException {
+
 		final String PATH = "src/main/resources/";
 		final String SOURCE = "example.jpg";
 		final String RESULTTYPE = "png";
@@ -25,7 +170,7 @@ public class myMain {
 		String dstName = null;
 
 		final int processors = Runtime.getRuntime().availableProcessors();
-		boolean parallel = false;
+		boolean parallel = seqPara;
 		int threadQuantity = 4;
 
 		BufferedImage image = ImageIO.read(new File(PATH + SOURCE));
@@ -34,10 +179,9 @@ public class myMain {
 
 		BufferedImage newImage = null;
 
-		int mode = 0;
 		int threshold = 128;
 		matrix myMatrix = null;
-		
+
 		switch (mode) {
 		case (1):
 			myMatrix = new matrix(new int[][] { { 0, 0, 4, 1 }, { 1, 4, 1, 0 }, { 0, 1, 0, 0 } }, threshold);
@@ -66,7 +210,7 @@ public class myMain {
 
 		try {
 			// maxThreads zur Info, kann (soll!) sp√§ter gel√∂scht werden - auch
-			// in den Objekten f√ºr die Paralelleverarveitung
+			// in den Objekten fuer die Paralelleverarveitung
 			int maxThreads = 0;
 			if (parallel) {
 
